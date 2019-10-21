ac4997a9801666d6da831e2311871937bc22f19b
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-10-21 17:39:48 +0300

    init

--- AVAudioPlayer/ContentView.swift
+++ AVAudioPlayer/ContentView.swift
@@ -7,10 +7,56 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    
+    @State var time: CGFloat = 0
+    @State var player: AVAudioPlayer!
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            ZStack(alignment: .leading, content: {
+                
+            
+                Capsule()
+                    .fill(Color.gray)
+                    .frame(height: 8)
+                    .padding(10)
+                
+                Capsule()
+                    .fill(Color.orange).frame(width: time, height: 8)
+                    .padding(10)
+            })
+            
+            Button(action: {
+                
+                self.player.play()
+                
+                DispatchQueue.global(qos: .background).async {
+                    
+                    while true {
+                        let screenWidth = UIScreen.main.bounds.width - 20
+                        
+                        let currenttime = self.player.currentTime / self.player.duration
+                        
+                        let timeForLabel = CGFloat(currenttime) * screenWidth
+                        
+                        self.time = timeForLabel
+                        
+                        //print(currenttime)
+                    }
+                }
+                
+            }) {
+                Text("Play")
+            }
+            
+        }.onAppear() {
+            let url = Bundle.main.path(forResource: "audio", ofType: "mp3")
+            
+            self.player = try! AVAudioPlayer(contentsOf:  URL(fileURLWithPath: url!))
+        }
     }
 }
 
